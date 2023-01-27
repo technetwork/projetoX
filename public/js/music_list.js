bdfe79a989d3756c88faa619205291b19ff51afa
technetwork <109860444+technetwork@users.noreply.github.com>
2023-01-27 14:16:11 -0300

    Update music_list.js

--- public/js/music_list.js
+++ public/js/music_list.js
@@ -16,5 +16,23 @@ const music_list = [
         name: "Trevo",
         artist: "Anavitória",
         music: "https://raw.githubusercontent.com/technetwork/projetoX/main/audio/Ana-Vitoria-Trevo.mp3"
+    },
+    {
+        img: "https://i.scdn.co/image/ab67616d0000b2733d74a82752df1c830347e8f6",
+        name: "Linda Demais",
+        artist: "Roupa Nova",
+        music: "https://github.com/technetwork/projetoX/raw/main/audio/Roupa-Nova-Linda-Demais.mp3"
+    },
+    {
+        img: "https://i.scdn.co/image/ab67616d0000b2731133d6a1e76c6b4b60c9e447",
+        name: "Meu Abrigo",
+        artist: "Melim",
+        music: "https://raw.githubusercontent.com/technetwork/projetoX/main/audio/Melim-Meu-Abrigo.mp3"
+    },
+    {
+        img: "https://i.scdn.co/image/ab67616d0000b2733d74a82752df1c830347e8f6",
+        name: "Dona",
+        artist: "Roupa Nova",
+        music: "https://github.com/technetwork/projetoX/raw/main/audio/Roupa-Nova-Dona.mp3"
     }
 ];
